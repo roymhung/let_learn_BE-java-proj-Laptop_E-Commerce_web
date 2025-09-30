5f524f7fb3922ba432f8b01c06637b9ea1e23b63
royhung <lemanhhung1555@gmail.com>
2025-09-30 22:29:22 +0700

    first commit

---
+++ src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/BeJavaProjLaptopECommerceWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.BE_java_proj_Laptop_E_Commerce_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaProjLaptopECommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaProjLaptopECommerceWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BE_java_proj_Laptop_E_Commerce_web/BeJavaProjLaptopECommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_java_proj_Laptop_E_Commerce_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaProjLaptopECommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
