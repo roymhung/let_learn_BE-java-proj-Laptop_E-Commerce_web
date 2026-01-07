10cb1c07d30f2bd56041fbd841eb568abcac7c24
royhung <lemanhhung1555@gmail.com>
2026-01-08 00:55:27 +0700

    #60. Repository hoàn thiện từ cấu trúc đến kết nối id database

--- src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/controller/UserController.java
+++ src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.BE_java_proj_Laptop_E_Commerce_web.domain.User;
-import com.example.BE_java_proj_Laptop_E_Commerce_web.repository.UserRepository;
 import com.example.BE_java_proj_Laptop_E_Commerce_web.service.UserService;
 
 
@@ -18,17 +17,15 @@ public class UserController {
 
     // DI : dependency injection
     private final UserService userService;
-    private final UserRepository userRepository;
 
-    public UserController(UserService userService, UserRepository userRepository) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userRepository = userRepository;
     }
 
     @RequestMapping("/")
     public String getHomepage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("roy", test);
+
+        model.addAttribute("roy", "test");
         model.addAttribute("hung", "haha2026");
         return "hello";
     }
@@ -43,7 +40,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserpage(Model model, @ModelAttribute("newUser") User royhung) {
         System.out.println(" Run here " + royhung);
-        this.userRepository.save(royhung);
+        this.userService.handleSaveUser(royhung);
 
         return "hello";
     }
--- src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/service/UserService.java
+++ src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/service/UserService.java
@@ -2,10 +2,20 @@ package com.example.BE_java_proj_Laptop_E_Commerce_web.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.BE_java_proj_Laptop_E_Commerce_web.domain.User;
+import com.example.BE_java_proj_Laptop_E_Commerce_web.repository.UserRepository;
+
 @Service
 public class UserService {
-    public String handleHello() {
-        return "hello from service";
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
+    public User handleSaveUser(User user) {
+        User mhung = this.userRepository.save(user);
+        System.out.println(mhung);
+        return mhung;
+    }
 }
