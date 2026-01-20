6b6531c81a8ca821692ca72b3087a400ff49e056
royhung <lemanhhung1555@gmail.com>
2026-01-20 18:21:14 +0700

    Done Yêu cầu dự án thực hành Phân Tích Model cho dự án Phân Tích Thiết Kế Database Design Models cho database

--- src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/domain/User.java
+++ src/main/java/com/example/BE_java_proj_Laptop_E_Commerce_web/domain/User.java
@@ -1,11 +1,18 @@
 package com.example.BE_java_proj_Laptop_E_Commerce_web.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +22,19 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+
+
+    // roleId
+    // User many -> to one -> role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
 
     public long getId() {
         return id;
@@ -64,11 +84,37 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName="
-                + fullName + ", address=" + address + ", phone=" + phone + "]";
+                + fullName + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar
+                + ", role=" + role + ", orders=" + orders + "]";
     }
+
 
 
 }
